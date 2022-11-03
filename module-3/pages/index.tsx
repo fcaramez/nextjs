51c8faceb21bf816b4cfa1228016a324705c5aa9
fcaramez <francisco.caramez.duarte@gmail.com>
2022-11-03 12:04:23 +0000

    starts implementing getStaticProps

--- module-3/pages/index.tsx
+++ module-3/pages/index.tsx
@@ -1,9 +1,31 @@
-export default function Home(props: any /*To Change!*/) {
+import React from "react";
+import { Product } from "../types/product.types";
+import fs from "fs/promises";
+import path from "path";
+
+const Home = ({ products }: { products: Array<Product> }) => {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product: Product) => {
+        return (
+          <li key={product.id}>
+            <h1>{product.title}</h1>
+          </li>
+        );
+      })}
     </ul>
   );
-}
+};
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+};
+export default Home;
