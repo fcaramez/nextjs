ed2be09f0f2d17343e23704c0f93826b0c22671f
fcaramez <francisco.caramez.duarte@gmail.com>
2022-11-07 22:32:56 +0000

    first api route, creates basic form with useRef

--- module-3/pages/index.tsx
+++ module-3/pages/index.tsx
@@ -1,7 +1,30 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInput = useRef<HTMLInputElement>(null);
+  const feedbackInput = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+
+    const email = emailInput.current?.value;
+    const feedback = feedbackInput.current?.value;
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" ref={emailInput} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackInput}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
