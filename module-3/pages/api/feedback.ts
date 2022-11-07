17e8e989706ae9a85cf4a060dc84b4b8ff5478ce
fcaramez <francisco.caramez.duarte@gmail.com>
2022-11-07 23:22:40 +0000

    good luck for future xico to debug this

--- module-3/pages/api/feedback.ts
+++ module-3/pages/api/feedback.ts
@@ -1,6 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: "Hello Next" });
+  if (req.method === "POST") {
+    const { email, text } = req.body;
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: text,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+
+    const fileData = fs.readFileSync(filePath);
+
+    const data = JSON.parse(fileData.toString());
+
+    data.push(data);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else res.status(200).json({ message: "Hello world" });
 };
+
 export default handler;
--- module-3/pages/index.tsx
+++ module-3/pages/index.tsx
@@ -1,20 +1,54 @@
-import { useRef } from "react";
+import axios from "axios";
+import { FormEvent, useRef } from "react";
 
 export default function Home() {
   const emailInput = useRef<HTMLInputElement>(null);
   const feedbackInput = useRef<HTMLTextAreaElement>(null);
 
-  const handleSubmit = (e: Event) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const email = emailInput.current?.value;
-    const feedback = feedbackInput.current?.value;
+    const email = emailInput.current?.value.toString();
+    const feedback = feedbackInput.current?.value.toString();
+
+    const body = {
+      email: email,
+      text: feedback,
+    };
+
+    console.log(body);
+
+    /* const options = {
+      method: "PSOT",
+      mode: "no-cors",
+      body: JSON.stringify(body),
+    }; */
+
+    axios
+      .post("http://localhost:3000/api/feedback", body)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+
+    /* fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        res.json();
+        console.log(res);
+      })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err)); */
   };
 
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Your email address</label>
           <input type="email" id="email" ref={emailInput} />
